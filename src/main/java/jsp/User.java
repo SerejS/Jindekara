8c317a2203715f425834f9ab4954fba69f854377
Sergey Sokolov <57752771+SerejS@users.noreply.github.com>
2020-10-28 18:54:55 +0300

    Реализация солёного хэширования пароля

--- src/main/java/jsp/User.java
+++ src/main/java/jsp/User.java
@@ -1,17 +1,20 @@
 package jsp;
 
+import org.apache.commons.codec.digest.DigestUtils;
 import javax.servlet.http.HttpServletRequest;
 import java.sql.*;
 import java.util.Enumeration;
+import java.security.SecureRandom;
 
 public class User {
+
     private static final String URL_BD = "jdbc:mysql://localhost:3306/mydbtest?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
     private static final String USERNAME_BD = "root";
     private static final String PASSWORD_BD = "root";
 
     private static boolean check(String log, String pas) {
         try {
-            Driver driver = new com.mysql.jdbc.Driver();
+            Driver driver = new com.mysql.cj.jdbc.Driver();
             DriverManager.registerDriver(driver);
         } catch (SQLException e) {
             e.printStackTrace();
@@ -20,7 +23,7 @@ public class User {
              Statement statement = connection.createStatement()) {
             ResultSet res = statement.executeQuery("SELECT * FROM users");
             while (res.next()) {
-                if (res.getString(2).equals(log) && res.getString(3).equals(pas)) {
+                if (res.getString(2).equals(log) && res.getString(3).equals(hash(pas, res.getString(4)))) {
                     return true;
                 }
 
@@ -33,16 +36,20 @@ public class User {
 
     private static void add(String log, String pas) {
         try {
-            Driver driver = new com.mysql.jdbc.Driver();
+            Driver driver = new com.mysql.cj.jdbc.Driver();
             DriverManager.registerDriver(driver);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        String query = "INSERT INTO users(name, pass) VALUES(?, ?)";
+
+        String query = "INSERT INTO users(name, password, salt) VALUES(?, ?, ?)";
         try (Connection connection = DriverManager.getConnection(URL_BD, USERNAME_BD, PASSWORD_BD);
              PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+
             preparedStatement.setString(1,log);
-            preparedStatement.setString(2, pas);
+            String salt = createSalt(); // Creating salt for password.
+            preparedStatement.setString(2, hash(pas, salt));
+            preparedStatement.setString(3, salt);
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -51,6 +58,7 @@ public class User {
 
     public static String register(HttpServletRequest req) {
         Enumeration<String> s = req.getParameterNames();
+
         if (s.hasMoreElements()) {
             if (User.check(req.getParameter("name"), req.getParameter("pass"))) {return  "Такой пользователь уже существуюет";}
             else {
@@ -63,5 +71,21 @@ public class User {
     public static boolean login(HttpServletRequest req) {
         Enumeration<String> s = req.getParameterNames();
         return s.hasMoreElements() && User.check(req.getParameter("name"), req.getParameter("pass"));
+    }
+
+    public static String createSalt() {
+        SecureRandom secureRandom = new SecureRandom();
+        StringBuilder salt = new StringBuilder();
+        byte [] randHex = new byte [8];
+
+        secureRandom.nextBytes(randHex);
+        for(byte i : randHex) {
+            salt.append(Integer.toHexString(i & 0xff));
+        }
+        return salt.toString();
+    }
+
+    public static String hash(String pass, String salt) {
+        return DigestUtils.sha256Hex(pass + salt);
     }
 }
