a205684b22cc1b2428742ef9e48b5af891afbd51
Sergey <sokolovserej@gmail.com>
2021-04-03 09:27:30 +0300

    Start Spring Project

---
+++ src/main/java/com/jindekara/JindekaraApplication.java
@@ -0,0 +1,13 @@
+package com.jindekara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JindekaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JindekaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jindekara/JindekaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jindekara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JindekaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
