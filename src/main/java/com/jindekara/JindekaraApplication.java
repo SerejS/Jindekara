330943aac69533efa19fe9a0d81d0fed78ac86b4
Sergey <sokolovserej@gmail.com>
2021-04-04 08:24:53 +0300

    Поключение первых страниц

--- src/main/java/com/jindekara/JindekaraApplication.java
+++ src/main/java/com/jindekara/JindekaraApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JindekaraApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(JindekaraApplication.class, args);
 	}
-
 }
--- src/main/java/com/jindekara/controllers/HelloController.java
+++ src/main/java/com/jindekara/controllers/HelloController.java
@@ -1,12 +1,22 @@
 package com.jindekara.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.HttpRequestHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
-    @RequestMapping("/")
-    public String index() {
-        return "Start!";
+    @GetMapping("/")
+    public String home_page() {
+        return "jindekara";
+    }
+
+    @GetMapping("/tales")
+    public String tales_page() {
+        return "tales";
     }
 }
